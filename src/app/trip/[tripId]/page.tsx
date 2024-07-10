d30df7f259037a53ae387d172f75c15553a26c82
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-10 17:09:20 -0300

    refactor: add components

--- src/app/trip/[tripId]/page.tsx
+++ src/app/trip/[tripId]/page.tsx
@@ -1,89 +1,61 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react'
+'use client'
+
+import Activities from '@/components/activities'
+import Button from '@/components/button'
+import CreateActivityModal from '@/components/create-activity-modal'
+import DestinationAndDateHeader from '@/components/destination-and-date-header'
+import Guests from '@/components/guests'
+import ImportantLinks from '@/components/important-links'
+import { Plus } from 'lucide-react'
+import { useState } from 'react'
 
 export default function TripDetails() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+    useState<boolean>(false)
+
+  function openCreateActivityModalOpen() {
+    setIsCreateActivityModalOpen(true)
+  }
+
+  function closeCreateActivityModalOpen() {
+    setIsCreateActivityModalOpen(false)
+  }
+
   return (
     <div className="mx-auto max-w-6xl space-y-8 px-6 py-10">
-      <div className="flex h-16 items-center justify-between rounded-xl bg-zinc-900 px-4 shadow-shape">
-        <div className="flex items-center gap-2">
-          <MapPin className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">Florianópolis, Brasil</span>
-        </div>
-
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400" />
-            <span className="text-zinc-100">17 a 23 de Agosto</span>
-          </div>
-
-          <div className="h-6 w-px bg-zinc-800" />
-
-          <button className="flex items-center gap-2 rounded-lg bg-zinc-800 px-5 py-2 font-medium text-zinc-200 hover:bg-zinc-700">
-            Alterar local/data
-            <Settings2 className="size-5" />
-          </button>
-        </div>
-      </div>
+      <DestinationAndDateHeader />
 
       <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button
+            <Button
+              onClick={openCreateActivityModalOpen}
+              variant="primary"
               type="button"
-              className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 hover:bg-lime-400"
             >
               <Plus className="size-5" />
               Cadastrar atividade
-            </button>
+            </Button>
           </div>
 
-          <div className="space-y-8">
-            <div className="space-y-2.5">
-              <div className="flex items-baseline gap-2">
-                <span className="text-xl font-semibold text-zinc-300">
-                  Dia 17
-                </span>
-                <span className="text-xs text-zinc-500">Sábado</span>
-              </div>
-              <p className="text-sm text-zinc-500">
-                Nenhuma atividade cadastrada nessa data.
-              </p>
-            </div>
-
-            <div className="space-y-2.5">
-              <div className="flex items-baseline gap-2">
-                <span className="text-xl font-semibold text-zinc-300">
-                  Dia 18
-                </span>
-                <span className="text-xs text-zinc-500">Domingo</span>
-              </div>
-              <div className="space-y-2.5">
-                <div className="flex items-center justify-between gap-3 rounded-xl bg-zinc-900 px-4 py-2.5 shadow-shape">
-                  <CircleCheck className="size-5 text-lime-300" />
-                  <span className="text-zinc-100">Academia em grupo</span>
-                  <span className="ml-auto text-sm text-zinc-400">08:00h</span>
-                </div>
-              </div>
-              <div className="space-y-2.5">
-                <div className="flex items-center justify-between gap-3 rounded-xl bg-zinc-900 px-4 py-2.5 shadow-shape">
-                  <CircleCheck className="size-5 text-lime-300" />
-                  <span className="text-zinc-100">Academia em grupo</span>
-                  <span className="ml-auto text-sm text-zinc-400">08:00h</span>
-                </div>
-              </div>
-              <div className="space-y-2.5">
-                <div className="flex items-center justify-between gap-3 rounded-xl bg-zinc-900 px-4 py-2.5 shadow-shape">
-                  <CircleCheck className="size-5 text-lime-300" />
-                  <span className="text-zinc-100">Academia em grupo</span>
-                  <span className="ml-auto text-sm text-zinc-400">08:00h</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Activities />
         </div>
 
-        <div className="w-80"></div>
+        <div className="w-80 space-y-6">
+          <ImportantLinks />
+
+          <div className="h-px w-full bg-zinc-800" />
+
+          <Guests />
+        </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <CreateActivityModal
+          closeCreateActivityModalOpen={closeCreateActivityModalOpen}
+        />
+      )}
     </div>
   )
 }
