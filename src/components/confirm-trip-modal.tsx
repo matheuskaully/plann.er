81b3d33b56678ce5abf381bed1c6d3ceb80e8d23
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-10 17:09:55 -0300

    feat: confirm trip modal

--- src/components/confirm-trip-modal.tsx
+++ src/components/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { Mail, User, X } from 'lucide-react'
 import { FormEvent } from 'react'
+import Button from './button'
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void
@@ -11,15 +12,15 @@ export default function ConfirmTripModal({
   closeConfirmTripModal,
 }: ConfirmTripModalProps) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/60">
+    <div className="fixed inset-0 flex items-center justify-center bg-black/60 backdrop-blur-md">
       <div className="w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <h2>Confirmar criação da viagem</h2>
             <button
               onClick={closeConfirmTripModal}
-              type="button"
               className="group rounded-lg p-2 hover:bg-lime-400"
+              type="button"
             >
               <X className="size-4 text-zinc-400 group-hover:text-lime-950" />
             </button>
@@ -58,12 +59,9 @@ export default function ConfirmTripModal({
             />
           </div>
 
-          <button
-            type="submit"
-            className="h-11 w-full justify-center rounded-lg bg-lime-300 px-5 font-medium text-lime-950 hover:bg-lime-400"
-          >
+          <Button size="full" type="submit">
             Confirmar criação da viagem
-          </button>
+          </Button>
         </form>
       </div>
     </div>
